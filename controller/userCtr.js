3425ba47034c099992859365fbab57948b221373
Anayo Oleru <anayo_oleru@outlook.com>
2019-01-09 06:01:50 -0800

    (feature):user should register

--- controller/userCtr.js
+++ controller/userCtr.js
@@ -43,7 +43,35 @@ const User = {
       }
       return res.status(400).send(error);
     }
-  }
+  },
+/**
+   * Login
+   * @param {object} req 
+   * @param {object} res
+   * @returns {object} user object 
+   */
+  async login(req, res) {
+    if (!req.body.email || !req.body.password) {
+      return res.status(400).send({'message': 'Some values are missing'});
+    }
+    if (!Helper.isValidEmail(req.body.email)) {
+      return res.status(400).send({ 'message': 'Please enter a valid email address' });
+    }
+    const text = 'SELECT * FROM users WHERE email = $1';
+    try {
+      const { rows } = await db.query(text, [req.body.email]);
+      if (!rows[0]) {
+        return res.status(400).send({'message': 'The credentials you provided is incorrect'});
+      }
+      if(!Helper.comparePassword(rows[0].password, req.body.password)) {
+        return res.status(400).send({ 'message': 'The credentials you provided is incorrect' });
+      }
+      const token = Helper.generateToken(rows[0].id);
+      return res.status(200).send({ token });
+    } catch(error) {
+      return res.status(400).send(error)
+    }
+}
 }
 
 module.exports = User;
